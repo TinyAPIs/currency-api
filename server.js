647268586fb88c282fc4afcf93ef06b8ac9abf64
Shashank Jha <shashankjha1994@gmail.com>
2023-07-21 03:37:25 +0530

    Prepare for Vercel deployment

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});

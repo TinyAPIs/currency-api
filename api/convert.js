292dcca667742f4ef8e2694394c68d793a2cafe9
Shashank Jha <shashankjha1994@gmail.com>
2023-07-22 20:32:01 +0530

    Added serverless func support

--- api/convert.js
+++ api/convert.js
@@ -6,22 +6,24 @@ module.exports = async (req, res) => {
     // URL of the static file in your Vercel deployment
     const fileUrl = `https://currency-api.tinyapi.co/data/${date.replace(/-/g, '/')}/currencies/${from_currency}.json`;
 
-    try {
-        // Fetch the data from the static file via an HTTP request
-        const response = await axios.get(fileUrl);
-        const data = response.data;
+    // Fetch the data from the static file via an HTTP request
+    axios.get(fileUrl)
+        .then(response => {
+            const data = response.data;
 
-        if (!data['exchange-rates'][to_currency]) {
-            return res.status(404).json({ error: `Exchange rate not found for the currency: ${to_currency}` });
-        }
+            if (!data['exchange-rates'][to_currency]) {
+                return res.status(404).json({ error: `Exchange rate not found for the currency: ${to_currency}` });
+            }
 
-        res.json({
-            date: data.date,
-            from: from_currency,
-            to: to_currency,
-            rate: data['exchange-rates'][to_currency]
+            res.json({
+                date: data.date,
+                from: from_currency,
+                to: to_currency,
+                rate: data['exchange-rates'][to_currency]
+            });
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).json({ error: 'An error occurred while fetching the data.' });
         });
-    } catch (error) {
-        return res.status(404).json({ error: 'Data not found for the given date and currency.' });
-    }
 };

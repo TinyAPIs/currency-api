a43b907d5520539e405fcbd347f216d96301bb99
Shashank Jha <shashankjha1994@gmail.com>
2023-07-22 19:36:47 +0530

    Potential fix for vercel deployment failure

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
 const app = require('../server'); // adjust the path as needed
-const { createServer } = require('@vercel/node-server');
-const server = createServer(app);
-module.exports = server;
+module.exports = app;

ee2805734d266272d0f7930600b8037bffe1ca61
Shashank Jha <shashankjha1994@gmail.com>
2023-07-22 20:07:09 +0530

    Added serverless func support

--- api/index.js
+++ api/index.js
@@ -1,33 +1,4 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit');
-const throttle = require('express-throttle');
-const apicache = require('apicache');
-const helmet = require('helmet');
-
-const app = express();
-
-// Apply rate limiting
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
-
-// Apply throttling
-app.use(throttle({
-    "rate": "50/s",      // how many requests per some time period
-    "burst": 10,        // max number of requests at a time
-    "ip": true          // throttle per IP
-}));
-
-// Apply caching
-let cache = apicache.middleware;
-app.use(cache('5 minutes'));
-
-// Apply security measures
-app.use(helmet());
-
-app.get('/', (req, res) => {
+module.exports = (req, res) => {
     res.json({
         message: 'Welcome to the Currency API! Use this API to get exchange rates between different currencies.',
         usage: {
@@ -39,6 +10,4 @@ app.get('/', (req, res) => {
             currency_conversion: '/api/2023-07-22/aed/inr/'
         }
     });
-});
-
-module.exports = app;
+};
